4ad0834bb25abc5a2bd7d02ebe9bbf7ae6262312
MTAttic <13680882+MTAttic@users.noreply.github.com>
2023-04-05 16:05:16 +0200

    Add comment

--- src/file_handler/txt_handler.rs
+++ src/file_handler/txt_handler.rs
@@ -1,7 +1,20 @@
+// Import necessary libraries
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+/// This function takes a file path as input and reads the contents of the file.
+/// It returns a vector of strings, where each string is a word from the file.
+///
+/// # Arguments
+///
+/// * `file_path` - A path to the file to be processed.
+///
+/// # Returns
+///
+/// * A `Result` object containing a `Vec` of `String`s, where each `String` is a word from the file
+///   if the file could be successfully read, otherwise an `io::Error` object.
+///
 pub fn process_txt(file_path: &Path) -> Result<Vec<String>, io::Error> {
     let file = File::open(file_path)?;
     let reader = io::BufReader::new(file);
