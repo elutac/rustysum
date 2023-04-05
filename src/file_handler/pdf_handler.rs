2135dd259f8d00847706ee24d4e8ae028db2b10a
MTAttic <13680882+MTAttic@users.noreply.github.com>
2023-04-05 15:30:09 +0200

    Add basic functionallity for txt, pdf extraction

--- src/file_handler/pdf_handler.rs
+++ src/file_handler/pdf_handler.rs
@@ -1,5 +1,18 @@
+use pdf_extract::extract_text;
+use std::io::{self, Error, ErrorKind};
 use std::path::Path;
 
-pub fn process_pdf(file_path: &Path) {
-    println!("{:?} is a PDF file.", file_path);
+pub fn process_pdf(file_path: &Path) -> Result<Vec<String>, io::Error> {
+    let text = extract_text(file_path).map_err(|e| {
+        Error::new(
+            ErrorKind::InvalidData,
+            format!("Failed to extract text from PDF: {}", e),
+        )
+    })?;
+
+    let text = text.replace("\r", "").replace("\n", " ").replace("\t", " ");
+    let text = text.split_whitespace().collect::<Vec<&str>>().join(" ");
+
+    let words: Vec<String> = text.split_whitespace().map(|s| s.to_string()).collect();
+    Ok(words)
 }
--- src/file_handler/txt_handler.rs
+++ src/file_handler/txt_handler.rs
@@ -1,5 +1,17 @@
+use std::fs::File;
+use std::io::{self, BufRead};
 use std::path::Path;
 
-pub fn process_txt(file_path: &Path) {
-    println!("{:?} is a text file.", file_path);
+pub fn process_txt(file_path: &Path) -> Result<Vec<String>, io::Error> {
+    let file = File::open(file_path)?;
+    let reader = io::BufReader::new(file);
+    let mut words: Vec<String> = Vec::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        let line_words: Vec<String> = line.split_whitespace().map(String::from).collect();
+        words.extend(line_words);
+    }
+
+    Ok(words)
 }
